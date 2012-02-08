7184236aaa20d073f1ace0f603668d0455ef7f8f
Maxim Starodubcev <starodubcevmax@gmail.com>
2012-02-08 22:48:42 +0200

    Autodetect video resolution.

--- video2rockbox.py
+++ video2rockbox.py
@@ -1,94 +1,19 @@
-#!/usr/bin/python3
+#!/usr/bin/env python
 #-*- coding: utf-8 -*-
 
+from config import *
 from optparse import OptionParser
 from os import system
+from subprocess import Popen, PIPE
 
 full_model_info = ()
-Q = 0.0
-AR = 0
-resolutions = (
-  ('320x240', '320x200', '320x180'),
-  ('220x166', '220x138', '220x124'),
-  ('176x128', '176x110', '176x100'),
-  ('160x120', '160x100', '160x90'),
-  ('138x104', '138x86', '138x78'),
-  ('128x96', '128x80', '128x72'),
-  ('106x80', '128x80', '132x74'),
-)
-models = {
-  'Archos':{
-    
-  },
-  'iRiver':{
-    10:(3, 'H120/H140'),
-    11:(1, 'H320/H340'),
-    14:(3, 'H10 20Gb'),
-    15:(5, 'H10 5/6Gb'),
-  }
-  'Apple iPod':{
-    20:(1, 'Color/Photo'),
-    21:(2, 'Nano 1G'),
-    22:(0, 'Video'),
-    24:(0, '4G Grayscale'), #?
-    25:(4, 'Mini 1G'),
-    26:(4, 'Mini 2G'),
-    27:(0, '1G, 2G'), #?
-    28:(2, 'Nano 2G'),
-  }
-  'Cowon/iAudio':{
-    30:(3, 'X5/X5V/X5L'),
-    31:(3, 'M5/M5L'),
-  }
-  'Toshiba':{
-    40:(0, 'Gigabeat F/X'),
-    41:(0, 'Gigabeat S'),
-  }
-  'SanDisk':{
-    50:(1, 'Sansa e200'),
-    52:(6, 'Sansa c200'),
-    56:(1, 'Sansa e200v2'),
-    58:(1, 'Sansa Fuze'),
-    63:(1, 'Sansa Fuze v2'),
-    64:(0, 'Sansa Fuze+'),
-  },
-  'Olympus':{
-    70:(0, 'M:Robe 100'), #?
-  },
-  'Logik':{
-    
-  },
-  'Creative':{
-    
-  },
-  'Philips':{
-    
-  },
-  'Meizu':{
-    
-  },
-  'Onda':{
-    
-  },
-  'Lyre project':{
-    
-  },
-  'Samsung':{
-    
-  },
-  'Tatung':{
-    
-  },
-  'Packard Bell':{
-    
-  },
-  'MPIO':{
-    
-  },
-  'HiFiMAN':{
-    
-  },
-}
+video_res = ()
+
+def GetResolution(infile):
+  com = 'ffmpeg -i "%s" 2>&1 | grep "Video:" | grep -Eo "[0-9]{2,4}x[0-9]{2,4}"' % infile
+  raw = Popen(com, shell=True, stdin=PIPE, stdout=PIPE).stdout.read()
+  out = str(raw).replace('\\n\'', '').replace('b\'', '').split('x')
+  return (int(out[0]), int(out[1]))
 
 parser = OptionParser()
 parser.add_option('-m', '--model', dest='model', help='Player model. For list all suported models: --models-list or -l.', metavar='<int>')
@@ -127,9 +52,22 @@ for manufacturer in models:
     full_model_info = models[manufacturer][model]
     break
 
-Q = 4/3
-AR = 0
+#Get video resolution and calculate resolution of out file
+video_res = GetResolution(input_file)
 
-system('ffmpeg -i "%s" -vcodec mpeg2video -b 100k -an -s %s -r 23 -mbd rd -trellis 2 -cmp 2 -subcmp 2 -g 100 -pass 1 "%s"' % (input_file, resolutions[full_model_info[0]][AR], output_file))
+if video_res:
+  video_ar = video_res[0] / video_res[1]
+  player_ar = resolutions[full_model_info[0]][0] / resolutions[full_model_info[0]][1]
 
-system('ffmpeg -i "%s" -vcodec mpeg2video -b %ik -ac 2 -ab %ik -acodec libmp3lame -s %s -r 23 -mbd rd -trellis 2 -cmp 2 -subcmp 2 -g 100 -pass 2 -y "%s"' % (input_file, video_rate, audio_rate, resolutions[full_model_info[0]][AR], output_file))
+if video_ar == player_ar:
+  resolution = '%ix%i' % resolutions[full_model_info[0]]
+elif video_ar > player_ar:
+  resolution = '%ix%i' % (resolutions[full_model_info[0]][0], int(resolutions[full_model_info[0]][0] / video_ar))
+else:
+  resolution = '%ix%i' % (int(resolutions[full_model_info[0]][1] * video_ar), resolutions[full_model_info[0]][1])
+
+
+#Convert video
+system('ffmpeg -i "%s" -vcodec mpeg2video -b 100k -an -s %s -r 23 -mbd rd -trellis 2 -cmp 2 -subcmp 2 -g 100 -pass 1 "%s"' % (input_file, resolution, output_file))
+
+system('ffmpeg -i "%s" -vcodec mpeg2video -b %ik -ac 2 -ab %ik -acodec libmp3lame -s %s -r 23 -mbd rd -trellis 2 -cmp 2 -subcmp 2 -g 100 -pass 2 -y "%s"' % (input_file, video_rate, audio_rate, resolution, output_file))
